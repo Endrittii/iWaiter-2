98c0681b2a74cf0806ecc8cdf542a1aa53856fde
IMac <imac@imacs-imac.local>
2019-07-18 16:01:18 +0200

    Initial Commit

---
+++ iWaiter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iWaiter
+//
+//  Created by IMac on 7/18/19.
+//  Copyright © 2019 Endrit Hoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
